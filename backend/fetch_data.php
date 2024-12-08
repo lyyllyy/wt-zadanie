14ecbc7c4e42c37093aec04c23b1672ab5395db6
lyyllyy <dinex1336@gmail.com>
2024-12-08 21:20:36 +0100

    .

---
+++ backend/fetch_data.php
@@ -0,0 +1,43 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json; charset=UTF-8");
+
+// Database credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "webove";
+
+// Connect to the database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die(json_encode(["error" => "Connection failed: " . $conn->connect_error]));
+}
+
+// Query to fetch data
+$sql = "SELECT id, name, birth_year, country, email, phone, status FROM users";
+$result = $conn->query($sql);
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Origin: http://localhost:3000");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    exit(0);
+}
+
+if ($result->num_rows > 0) {
+    $users = [];
+    while ($row = $result->fetch_assoc()) {
+        $users[] = $row;
+    }
+    echo json_encode($users);
+} else {
+    echo json_encode([]);
+}
+
+$conn->close();
+?>
